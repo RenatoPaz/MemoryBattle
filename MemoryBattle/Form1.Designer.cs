2aeaf151aa6939dc90bc6df4a18ab50b967e81f5
RenatoPaz <renatogomesdapaz01@gmail.com>
2025-09-24 14:48:03 -0600

    Test Push

--- MemoryBattle/Form1.Designer.cs
+++ MemoryBattle/Form1.Designer.cs
@@ -1,40 +1,71 @@
 ﻿namespace MemoryBattle
+//Test comment 2
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSinglePlayer;
+        private System.Windows.Forms.Button btnBattleMode;
+        private System.Windows.Forms.Button btnScoreInfo;
+        private System.Windows.Forms.Button btnHowToPlay;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSinglePlayer = new System.Windows.Forms.Button();
+            this.btnBattleMode = new System.Windows.Forms.Button();
+            this.btnScoreInfo = new System.Windows.Forms.Button();
+            this.btnHowToPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1 (Título)
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(120, 30);
+            this.label1.Text = "Memory Battle";
+            // 
+            // btnSinglePlayer
+            // 
+            this.btnSinglePlayer.Location = new System.Drawing.Point(100, 100);
+            this.btnSinglePlayer.Size = new System.Drawing.Size(200, 40);
+            this.btnSinglePlayer.Text = "1 Player";
+            this.btnSinglePlayer.Click += new System.EventHandler(this.btnSinglePlayer_Click);
+            // 
+            // btnBattleMode
+            // 
+            this.btnBattleMode.Location = new System.Drawing.Point(100, 160);
+            this.btnBattleMode.Size = new System.Drawing.Size(200, 40);
+            this.btnBattleMode.Text = "Battle Mode";
+            this.btnBattleMode.Click += new System.EventHandler(this.btnBattleMode_Click);
+            // 
+            // btnScoreInfo
+            // 
+            this.btnScoreInfo.Location = new System.Drawing.Point(100, 220);
+            this.btnScoreInfo.Size = new System.Drawing.Size(200, 40);
+            this.btnScoreInfo.Text = "Score Info";
+            this.btnScoreInfo.Click += new System.EventHandler(this.btnScoreInfo_Click);
+            // 
+            // btnHowToPlay
+            // 
+            this.btnHowToPlay.Location = new System.Drawing.Point(100, 280);
+            this.btnHowToPlay.Size = new System.Drawing.Size(200, 40);
+            this.btnHowToPlay.Text = "How to Play";
+            this.btnHowToPlay.Click += new System.EventHandler(this.btnHowToPlay_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(420, 380);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSinglePlayer);
+            this.Controls.Add(this.btnBattleMode);
+            this.Controls.Add(this.btnScoreInfo);
+            this.Controls.Add(this.btnHowToPlay);
+            this.Name = "Form1";
+            this.Text = "Memory Battle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
-
-        #endregion
     }
 }
-
--- MemoryBattle/Form1.cs
+++ MemoryBattle/Form1.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//TestpushGitHub
 namespace MemoryBattle
 {
     public partial class Form1 : Form
@@ -15,6 +8,28 @@ namespace MemoryBattle
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //test comment
+
+        private void btnSinglePlayer_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Starting Single Player...");
+        }
+
+        private void btnBattleMode_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Starting Battle Mode...");
+        }
+
+        private void btnScoreInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Scores will be displayed here.");
+        }
+
+        private void btnHowToPlay_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Match pairs of cards before the timer runs out!");
         }
     }
 }
