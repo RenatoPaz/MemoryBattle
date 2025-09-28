04ff118fd7674124a8cf540c654a5bb3ca1fae70
RenatoPaz <renatogomesdapaz01@gmail.com>
2025-09-28 16:08:56 -0600

    Button how to play

--- MemoryBattle/Form1.cs
+++ MemoryBattle/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 //TestpushGitHub
 namespace MemoryBattle
@@ -29,7 +30,13 @@ namespace MemoryBattle
 
         private void btnHowToPlay_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Match pairs of cards before the timer runs out!");
+            //MessageBox.Show("Match pairs of cards before the timer runs out!");
+            string instructions = "Match pairs of cards.\n\n" +
+                      "Players take turns; a correct match earns another try.\n\n" +
+                      "Each new level adds more pairs and harder symbols.\n\n" +
+                      "The player with the most pairs wins.";
+
+            MessageBox.Show(instructions, "How to Play", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
